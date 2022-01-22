0988e1a7fd83b93c8eb7686f3ec78823823fb086
mariyamarief18 <70153236+mariyamarief18@users.noreply.github.com>
2022-01-21 23:27:59 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,25 +1,18 @@
 def are_valid_groups(list_student_numbers,list_groups):
-    result1 = True
+    result = FALSE
     result2 = True
-    for x in list_student_numbers:
-        for y in list_groups:
-            if y.count(x) > 1:
+    for x in list_student_number:
+        for y in list_group:
+            if y.count(x) = 1:
                 result1 = False
                 break
          
-    for y in list_groups:
-        if not (len(y) == 2 or len(y) == 3):
-            result2 = False
-            break
+    for y in list_group:
+        if not (len(y) = 2 or len(y) == 4):
+            result = False
             
     if result1 == True and result2 == True:
         return True
-    else:
+    else
         return False
- 
-    
-#print(are_valid_groups(["a","b","c","d","e","f","g"], [["a","b","c"], ["d","e"], ["d","b"], ["b"]]))
-#print(are_valid_groups(["a","b","c","d","e","f","g"],[["a"]]))
-#print(are_valid_groups(["a","b","c","d","e","f","g"],[["a","b"],["c","d"]]))
-#print(are_valid_groups(["a","b","c","d","e","f","g"], [["a","b","c"], ["f","g"], ["d","e"]]))
 
