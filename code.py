663e74f257e3654b46cdf7141f3353e661ef9463
oliii23 <70110454+oliii23@users.noreply.github.com>
2022-01-21 21:50:38 -0500

    Add files via upload

--- code.py
+++ code.py
@@ -1,8 +1,6 @@
 def are_valid_groups():
-    list_student_numbers = []
-    list_groups = []
     for x in list_student_numbers:
         for y in list_groups:
-            if x == y:
+            if x in y:
                 result = True
-                return result
+    return result
