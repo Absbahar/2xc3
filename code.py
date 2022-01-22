f80b08afccdcc8b31116e53b54a99badbc748a74
oliii23 <70110454+oliii23@users.noreply.github.com>
2022-01-21 23:06:46 -0500

    Add files via upload

--- code.py
+++ code.py
@@ -1,8 +1,25 @@
-
-def are_valid_groups(list_student_numbers, list_groups):
-    result = False
+def are_valid_groups(list_student_numbers,list_groups):
+    result1 = True
+    result2 = True
     for x in list_student_numbers:
         for y in list_groups:
-            if x in y:
-                result = True
-    return result
+            if y.count(x) > 1:
+                result1 = False
+                break
+         
+    for y in list_groups:
+        if not (len(y) == 2 or len(y) == 3):
+            result2 = False
+            break
+            
+    if result1 == True and result2 == True:
+        return True
+    else:
+        return False
+ 
+    
+#print(are_valid_groups(["a","b","c","d","e","f","g"], [["a","b","c"], ["d","e"], ["d","b"], ["b"]]))
+#print(are_valid_groups(["a","b","c","d","e","f","g"],[["a"]]))
+#print(are_valid_groups(["a","b","c","d","e","f","g"],[["a","b"],["c","d"]]))
+#print(are_valid_groups(["a","b","c","d","e","f","g"], [["a","b","c"], ["f","g"], ["d","e"]]))
+
