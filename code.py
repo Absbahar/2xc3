4c0c8cecf263da5a249a663260ae0429d6bc0fb0
oliii23 <70110454+oliii23@users.noreply.github.com>
2022-01-21 23:18:53 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,19 +1,19 @@
 def are_valid_groups(stu_no, groups):
-    ans1 = True 
-    ans2 = True
+    ans1 = False
+    ans2 = False
     ans = False
     for i in stu_no:
         for j in groups:
-            if j.count(i) > 1:
+            if j.count(i) < 1:
                 ans1 = False
                 break
                 
     for j in groups:
-        if not (len(j) == 2 or len(j) == 3):
+        if not (len(j) == 4 or len(j) == 3):
             ans2 = False
             break
             
-    if ans1 == True and ans2 == True:
+    if ans1 == False and ans2 == True:
         ans = True
         
     return ans
